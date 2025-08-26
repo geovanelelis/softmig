a1e7daf75aeb71945a4a80f4a42120ecbf610961
Geovane <geovanelelisds@gmail.com>
2025-08-26 09:28:29 -0300

    Commit inicial do projeto

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import "../styles/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Softmig",
+  description: "Softmig Software e Telecomunicações",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
